711fc5063bedc93f264bff5ab20ef46d5c3114fd
lixingzh <lixing.zhang@outlook.com>
2016-07-14 21:25:49 -0600

    Initial Commit

---
+++ GoogleInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoogleInterview
+//
+//  Created by Lixing Zhang on 7/14/16.
+//  Copyright © 2016 Lixing Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
